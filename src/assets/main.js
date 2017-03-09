721cb632ba63d22bdae040cfeefdc8390186f5c3
Kenan Horoz <kenan.horoz@clear-it.de>
2017-03-09 10:37:18 +0100

    Kevins code reinkopiert

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,67 +2,71 @@ let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
-  let input = document.getElementById('user-guess');
-  if(answer.value == '' || attempt.value == ''){
-    setHiddenFields();
-  }
+    let input = document.getElementById('user-guess');
+    //add functionality to guess function here
+    if(answer.value == '' || attempt.value == ''){
+      setHiddenFields();
+    }
 
+   if(!validateInput(input.value)){
+      return(false);
+    }
+    attempt.value++;
 
-if(!validateInput(input.value)){
-  return;
-}
-attempt.value++;
-if(getResults(input.value)){
-  setMessage('You Win! :)');
-  showAnswer(true);
-  showReplay();
-} else if (attempt.value >= 10){
-  setMessage('You Lose! :()');
-} else {
-  setMessage('Incorrect, try again.');
-  }
+   if(getResults(input.value)) {
+      setMessage('You Win :)');
+      showAnswer(true);
+      showReplay();
+    } else if(attempt.value >= 10){
+      setMessage('You Lose! :(');
+      showAnswer(false);
+      showReplay();
+    }else {
+      setMessage('Incorrect, try again.');
+    }
 }
 
 function getResults(input){
   let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
-  for (i = 0; i < input.length; i++){
+  for(i = 0; i < input.length; i++) {
     if(input.charAt(i) == answer.value.charAt(i)){
-      html += '<span class="glyphicon glyphicon-ok"></span>';
-    } else if (answer.value.indexOf(input.CharAt(i)) > -1) {
+      html += 'span class="glyphicon glyphicon-ok"></span>';
+    } else if(answer.value.indexOf(input.charAt(i)) > -1){
       html += '<span class="glyphicon glyphicon-transfer"></span>';
-    } else {
-      html += ' <span class="glyphicon glyphicon-remove"></span>';
+    } else{
+      html += '<span class="glyphicon glyphicon-remove"></span>';
     }
   }
-  html += '</div></div>';
+
+ html += '</div></div>';
   document.getElementById('results').innerHTML += html;
 
-  if(input == answer.value){
+ if(input == answer.value){
     return true;
-  } else {
-    return false;
+  } else{
+  return false;
   }
 }
 
 //implement new functions here
-function setHiddenFields() {
-  answer.value = Math.floor(Math.random() * 10000).toString;
-  while(answer.value.length < 4){
-    answer.value = "0" + answer.value;
-  }
-  attempt.value = "0";
+function setHiddenFields(){
+    answer.value = Math.floor(Math.random() * 10000).toString();
+    while(answer.value.length < 4){
+      answer.value = "0" + answer.value;
+    }
+    attempt.value = "0";
 }
 
 function setMessage(message){
   document.getElementById('message').innerHTML = message;
 }
 
-function showAnswer(sucess){
+function showAnswer(succes) {
   let code = document.getElementById('code');
   if(success) {
-    code.className += 'sucess';
-  } else {
-    code.class className += 'failure';
+    code.className += ' succes';
+  }else {
+    code.className += ' failure';
   }
   code.innerHTML = answer.value;
 }
@@ -72,13 +76,11 @@ function showReplay(){
   document.getElementById('replay-div').style.display = "block";
 }
 
-
-function validateInput(input){
-  if (input.length != 4) {
-    setMessage("Guesses must be exactly 4 characters long.");
+function validateInput (input) {
+  if(input.length > 4){
+    setMessage('Guesses mus be exactly 4 characters long.');
     return false;
-  }
-  else {
-    return true;
+  }else {
+  return true;
   }
 }
